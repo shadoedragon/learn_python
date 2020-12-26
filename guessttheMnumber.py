5ba9a5028f812e5046aba76699c77236aacbb5f7
shadoedragon <zhao.yichen@outlook.com>
2020-12-26 11:30:16 -0500

    minigame

---
+++ guessttheMnumber.py
@@ -0,0 +1,20 @@
+import random
+print("="*42)
+print("Welcome to the guess the M number minigame")
+print("="*42)
+myst = random.randint(1,100)
+#print(myst)
+guess_it=0
+tries=0
+while guess_it != myst and tries < 7:
+    guess_it = input("please try guess: ")
+    if guess_it > myst:
+        print("it's bigger than")
+    elif guess_it < myst:
+        print ("it's smaller than")
+    tries = tries + 1
+if guess_it == myst:
+    print("Congratulation, you've completed the guess the M. number mini game")
+else:
+    print("sorry, you didn't complet the guess the M. number mini game,try again")
+    print("the number is ", myst)
