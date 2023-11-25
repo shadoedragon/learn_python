49d3393f32f77a2192eca16615e61cb5e3b32145
Eason <74150531+shadoedragon@users.noreply.github.com>
2023-11-24 20:16:55 -0500

    positive ratio and other things

--- CrashCoursePython/CH.02/full_name.py
+++ CrashCoursePython/CH.02/full_name.py
@@ -1,10 +1,11 @@
+# remove the number sign on one (ONE AT A TIME) of the codes to make the code work
 first_name = "ada"
 last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 message = f"Hello, {full_name.title()}!"
 
-message = f"Hello, {full_name.title()}!"
-print(message)
+# message = f"Hello, {full_name.title()}!"
+# print(message)
 
 # print(f"Hello, {full_name.title()}!")
 
