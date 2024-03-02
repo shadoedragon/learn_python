3b9047e36d918cc9694f0bfdc57299918ccb8ba7
Eason <74150531+shadoedragon@users.noreply.github.com>
2024-03-02 18:49:53 -0500

    do it yourself in ch.4 completed!

---
+++ CrashCoursePython/CH.04/animals.py
@@ -0,0 +1,5 @@
+pets = ['Cats', 'Dogs', 'Hamsters']
+for pet in pets:
+    print(f"{pet.title()} will make a very good pet!")
+
+print("Any of these animals will keep you great company!")
---
+++ CrashCoursePython/CH.04/pizzas.py
@@ -0,0 +1,6 @@
+pizzas = ['Margherita pizzas', 'New York style pizzas', 'Hawaiian pizzas']
+for pizza in pizzas:
+    print(f"I like {pizza.title()}")
+    print(f" {pizza.title()}, are very delicious")
+
+print("I really love all types of pizzas!")
